9ce424dc1918f436dbf53c0c1b1f781257d8e9a8
Saul Granillo <sgranillo@norson.net>
2021-01-21 20:50:34 -0700

    fin seccion 4

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon:string;
+  name: string;
+  redirecTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Accion',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirecTo: '/alert'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
